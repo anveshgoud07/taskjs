69e468aa90c580d856c42388ab6932ad5b08488a
vikas <vikas@email.com>
2026-04-17 15:00:04 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,88 @@
+//Task 1:Variable Update
+var myVar=50
+myVar=100
+console.log(myVar)
+
+
+//Task 2:Let Variable Path
+let marks=80
+marks=marks+10
+console.log(marks)
+
+
+//Task 3:Const Value
+const price=500
+const finalPrice=price+100
+console.log(finalPrice)
+
+
+//Task 4:Printing Statements
+alert("Welcome Team")
+console.log("Welcome Team")
+let year=2026
+console.log(year)
+let userName=confirm("Do you want to continue?")
+console.log(userName)
+
+
+//task5 :Dtatype check 
+let skill="JavaScript"
+console.log(typeof(skill))
+let a=250
+console.log(typeof(a))
+let boo=false
+console.log(typeof(boo))
+
+
+//task 6:Array Task
+let Fruits=["Apple","Banana","Mango","Guava","Orange"]
+console.log(Fruits)
+console.log(Fruits[0])
+console.log(Fruits[2])
+
+
+// Task 7:Object Task 
+let student={
+    name:"Naveen",
+    age:20,
+    course:"MERN"
+}
+console.log(student.name)
+console.log(student.age)
+
+
+//Tak 8:Arithmetic Operators
+console.log(20+10) 
+console.log(50-25)
+console.log(5*5)
+console.log(100/4)
+console.log(20%3)
+
+
+//Task9: Increment/Decrement
+let x=5
+x++;
+console.log(x)
+let y=10
+y--;
+console.log(y)
+
+
+//Task 10: Comparison Operators
+console.log(10>5)
+console.log(5<2)
+console.log(20=="20")
+console.log(15==="15")
+console.log(10!=8)
+
+
+//Task 11:Logical Operators
+
+console.log(5>2 && 10>3)
+console.log(7<5 ||8>2)
+console.log(!(10>5))
+
+
+// Task12: Ternary Operator
+let age=18
+age>=18?console.log("Eligible"):console.log("Not Eligible")
